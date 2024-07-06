1b88960d2609d957fad38579e2b4873bbfce2bf1
Ahammedb <ahammedb@voizzit.com>
2024-07-06 15:36:19 +0400

    latest

---
+++ src/app/[lang]/@auth/(.)login/page.js
@@ -0,0 +1,10 @@
+import ModalLogin  from '@/components/ModalLogin'
+import LoginWidget from '@/widgets/LoginWidget'
+
+export default function Page() {
+    return (
+        <ModalLogin>
+            asd
+        </ModalLogin>
+    )
+}
